fd2f389429c9420b448f85ae272643c8caa8dd44
Your Name <jasonbataillon@gmail.com>
2025-02-01 17:41:30 -0500

    Created and finished project.

---
+++ api/playlists.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const prisma = require('../prisma');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const playlists = await prisma.playlist.findMany();
+    res.json(playlists);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const playlist = await prisma.playlist.findUnique({
+      where: { id: +id },
+      include: { tracks: true },
+    });
+    if (playlist) {
+      res.json(playlist);
+    } else {
+      next({ status: 404, message: `${id} playlist doesn't exist.` });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const { name, description, trackIds, ownerId } = req.body;
+
+  if (!name || !description) {
+    return next({
+      status: 400,
+      message: 'Both fields must be provided.',
+    });
+  }
+
+  try {
+    const track = trackIds.map((id) => ({ id: +id }));
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        ownerId: +ownerId,
+        trackId: +trackIds,
+        track: { connect: track },
+      },
+      include: {
+        owner: true,
+        track: true,
+      },
+    });
+    res.status(201).json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
---
+++ prisma/seed.js
@@ -0,0 +1,52 @@
+const prisma = require('../prisma');
+const numUser = 5 + Math.floor(Math.random() * 5);
+const numPlaylist = 10 + Math.floor(Math.random() * 10);
+const numTrack = 20 + Math.floor(Math.random() * 20);
+
+const seed = async (
+  numUsers = numUser,
+  numPlaylists = numPlaylist,
+  numTracks = numTrack
+) => {
+  const users = Array.from({ length: numUsers }, (_, i) => ({
+    username: `User ${i}`,
+  }));
+  await prisma.user.createMany({ data: users });
+
+  const tracks = Array.from({ length: numTracks }, (_, j) => ({
+    name: `Track ${j}`,
+  }));
+  await prisma.track.createMany({ data: tracks });
+
+  const playlists = Array.from({ length: numPlaylists }, (_, l) => {
+    const ownerId = 1 + Math.floor(Math.random() * numUsers);
+    return {
+      name: `Playlist ${l}`,
+      description: `Anything for ${l}.`,
+      ownerId,
+    };
+  });
+  await prisma.playlist.createMany({ data: playlists });
+
+  for (let i = 0; i < numPlaylists; i++) {
+    const tracksTotal = 20 + Math.floor(Math.random() * numTracks);
+
+    const trackIds = Array.from({ length: tracksTotal }, () => ({
+      id: 1 + Math.floor(Math.random() * numTracks),
+    }));
+
+    await prisma.playlist.update({
+      where: { id: i + 1 },
+      data: {
+        tracks: { connect: trackIds.map((track) => ({ id: track.id })) },
+      },
+    });
+  }
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
